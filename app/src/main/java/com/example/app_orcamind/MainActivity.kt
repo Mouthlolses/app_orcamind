ca55a355cf131f32dcfad41441fb649e363438d1
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-07-15 00:28:36 -0300

    HomeScreen

--- app/src/main/java/com/example/app_orcamind/MainActivity.kt
+++ app/src/main/java/com/example/app_orcamind/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.app_orcamind.ui.screens.home.MainScreen
 import com.example.app_orcamind.ui.screens.login.LoginScreen
 
 class MainActivity : ComponentActivity() {
